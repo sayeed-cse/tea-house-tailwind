6525f4112f4fa4fc6e58d5b1a2c15e31f575fff4
sayeed-cse <sayeed-web-dev@gmail.com>
2023-04-14 03:02:11 +0600

    banner done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'man-rope': ['Manrope', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
